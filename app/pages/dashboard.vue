cb2060f0cabccedd18ceb29431f4640ae9d232ad
Faqih001 <fakiiahmad001@gmail.com>
2026-04-01 02:24:20 +0300

    feat: implement authentication middleware and dashboard layout for route protection

---
+++ app/pages/dashboard.vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtLayout name="dashboard">
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+<script setup lang="ts">
+// Parent route for all /dashboard pages
+// Also you can enforce auth middleware here for the whole dashboard
+definePageMeta({
+  middleware: 'auth'
+})
+</script>
