50ae4216bec533a46d82d75476d9f01f0ca95e31
Faqih001 <fakiiahmad001@gmail.com>
2026-04-01 02:24:57 +0300

    feat: refactor auth and sidebar stores; move to centralized location

--- app/pages/auth/register.vue
+++ app/pages/auth/register.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { z } from 'zod'
-import { useAuthStore } from '../../dashboard/stores/auth'
+import { useAuthStore } from '../../stores/auth'
 
 definePageMeta({
   layout: false
